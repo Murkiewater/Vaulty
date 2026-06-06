644df23d9afd2b3454d8a053179450b255b11eaf
Murkiewater <kekys492@gmail.com>
2026-06-06 19:40:42 +0500

    first setup, infinite loop between main window + sign-in

---
+++ app/(dashboard)/[StoreId]/(routes)/page.tsx
@@ -0,0 +1,9 @@
+const DashboardPage = () => {
+    return (
+        <div>
+            this is our dashboard!
+        </div>
+    );
+}
+
+export default DashboardPage;
---
+++ components/models/store-model.tsx
@@ -0,0 +1,90 @@
+"use client";
+
+import * as z from "zod";
+import { useForm } from "react-hook-form"
+import { zodResolver } from "@hookform/resolvers/zod";
+import { useState } from "react";
+import { toast } from "react-hot-toast";
+import axios from "axios";
+
+import { useStoreModel } from "@/hooks/use-store-model";
+import { Model } from "@/components/ui/model";
+import { Form, FormControl, FormField, FormItem, FormLabel, FormMessage } from "@/components/ui/form";
+import { Input } from "@/components/ui/input";
+import { Button } from "@/components/ui/button";
+
+const formSchema = z.object({
+    name: z.string().min(1),
+})
+
+export const StoreModel = () => {
+    const storeModel = useStoreModel();
+
+    const [ loading, setLoading ] = useState(false);
+
+    const form = useForm<z.infer<typeof formSchema>>({
+        resolver: zodResolver(formSchema),
+        defaultValues: {
+            name: "",
+        },
+    });
+ 
+    const onSubmit = async (values: z.infer<typeof formSchema>) => {
+        try {
+            setLoading(true);
+
+            const response = await axios.post('/api/stores', values);
+
+            toast.error("Campaign created!")
+        } catch (error) {
+            toast.error("Something went wrong...")
+        } finally {
+            setLoading(false);
+        }
+    }
+
+    return (
+        <Model title="Create a campaign"
+        description="Add a new marketing campaign"
+        isOpen={storeModel.isOpen}
+        onClose={storeModel.onClose}>
+            <div>    
+                <div className="space-y-4 py-2 pb-4">
+                    <Form {...form}>
+                        <form onSubmit={form.handleSubmit(onSubmit)}>
+                            <FormField 
+                            control={form.control} 
+                            name="name" 
+                            render={({ field }) => (
+                                <FormItem>
+                                    <FormLabel>Name</FormLabel>
+                                    <FormControl>
+                                        <Input
+                                        disabled={loading}
+                                        placeholder="Project" {...field}
+                                        />
+                                    </FormControl>
+                                    <FormMessage />
+                                </FormItem>
+                            )}
+                            /> 
+                            <div className="pt-6 space-x-2 flex items-center justify-end w-full">
+                                <Button 
+                                disabled={loading}
+                                variant="outline"
+                                onClick={storeModel.onClose}>
+                                    Cancel
+                                </Button>
+                                <Button 
+                                disabled={loading} 
+                                type="submit">
+                                    Continue
+                                </Button>
+                            </div>
+                        </form>
+                    </Form>
+                </div>
+            </div>
+        </Model>
+    );
+};
