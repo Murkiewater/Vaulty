54792cddd86ffac2f24005c42ea233e3eb229991
Murkiewater <kekys492@gmail.com>
2026-06-10 03:32:03 +0500

    backend fully done (orders + products + colors + sizes added)

--- components/ui/image-upload.tsx
+++ components/ui/image-upload.tsx
@@ -26,7 +26,7 @@ const ImageUpload: React.FC<ImageUploadProps> = ({
     }, []);
 
     const onUpload = (result: any) => {
-        onChange(result.info.secure_url);
+        console.log("uploading:", result.info.secure_url);
     }
     
     if (!isMounted) {
